e2ce0ab46ba9a1e90b63e29ac8ee8c6b231afa60
bitacademy-poscodx09 <bitacademy.poscodx@gmail.com>
2024-12-31 14:33:55 +0900

    Repository Refactoring #2

--- guestbook03/src/main/java/guestbook/repository/template/JdbcContext.java
+++ guestbook03/src/main/java/guestbook/repository/template/JdbcContext.java
@@ -3,9 +3,14 @@ package guestbook.repository.template;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.RowMapper;
 
 public class JdbcContext {
 	private DataSource dataSource;
@@ -14,6 +19,15 @@ public class JdbcContext {
 		this.dataSource = dataSource;
 	}
 	
+	public <E> List<E> queryForList(String sql, RowMapper<E> rowMapper) {
+		return queryForListWithStatementStrategy(new StatementStrategy() {
+			@Override
+			public PreparedStatement makeStatement(Connection connection) throws SQLException {
+				return connection.prepareStatement(sql);
+			}
+		}, rowMapper);
+	}
+
 	public int excuteUpdate(String sql, Object[] parameters) {
 		return executeUpdateWithStatementStrategy(new StatementStrategy() {
 			@Override
@@ -29,6 +43,24 @@ public class JdbcContext {
 		});
 	}
 	
+	private <E> List<E> queryForListWithStatementStrategy(StatementStrategy statementStrategy, RowMapper<E> rowMapper) {
+		List<E> result = new ArrayList<>();
+		
+		try (
+			Connection conn = dataSource.getConnection();
+			PreparedStatement pstmt = statementStrategy.makeStatement(conn);
+			ResultSet rs = pstmt.executeQuery();
+		) {
+			while(rs.next()) {
+				E e = rowMapper.mapRow(rs, rs.getRow());
+				result.add(e);
+			}
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+		
+		return result;
+	}
 	
 	private int executeUpdateWithStatementStrategy(StatementStrategy statementStrategy) {		
 		int count = 0;
