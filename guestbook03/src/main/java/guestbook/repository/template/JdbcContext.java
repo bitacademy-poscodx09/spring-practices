f9e26e0b2241b0b432e569d366e85dc9ae251a02
bitacademy-poscodx09 <bitacademy.poscodx@gmail.com>
2025-01-08 10:59:02 +0900

    jdbcContext update

--- guestbook03/src/main/java/guestbook/repository/template/JdbcContext.java
+++ guestbook03/src/main/java/guestbook/repository/template/JdbcContext.java
@@ -1,4 +1,3 @@
-
 package guestbook.repository.template;
 
 import java.sql.Connection;
@@ -7,29 +6,41 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.sql.DataSource;
-
 import org.springframework.jdbc.core.RowMapper;
 
 public class JdbcContext {
-	private DataSource dataSource;
+	private final DataSource dataSource;
 	
 	public JdbcContext(DataSource dataSource) {
 		this.dataSource = dataSource;
 	}
 	
-	public <E> List<E> queryForList(String sql, RowMapper<E> rowMapper) {
-		return queryForListWithStatementStrategy(new StatementStrategy() {
+	public <E> List<E> query(String sql, RowMapper<E> rowMapper) {
+		return queryWithStatementStrategy(new StatementStrategy() {
 			@Override
 			public PreparedStatement makeStatement(Connection connection) throws SQLException {
 				return connection.prepareStatement(sql);
 			}
 		}, rowMapper);
 	}
+	
+	public <E> E queryForObject(String sql, Object[] parameters, RowMapper<E> rowMapper) {
+		return queryForObjectWithStatementStrategy(new StatementStrategy() {
+			@Override
+			public PreparedStatement makeStatement(Connection connection) throws SQLException {
+				PreparedStatement pstmt = connection.prepareStatement(sql);
+				for(int i = 0; i < parameters.length; i++) {
+					pstmt.setObject(i+1, parameters[i]);
+				}
+				
+				return pstmt;
+			}
+		}, rowMapper);
+	}
 
-	public int excuteUpdate(String sql, Object[] parameters) {
-		return executeUpdateWithStatementStrategy(new StatementStrategy() {
+	public int update(String sql, Object... parameters) {
+		return updateWithStatementStrategy(new StatementStrategy() {
 			@Override
 			public PreparedStatement makeStatement(Connection connection) throws SQLException {
 				PreparedStatement pstmt = connection.prepareStatement(sql);
@@ -43,7 +54,7 @@ public class JdbcContext {
 		});
 	}
 	
-	public <E> List<E> queryForListWithStatementStrategy(StatementStrategy statementStrategy, RowMapper<E> rowMapper) throws RuntimeException {
+	private <E> List<E> queryWithStatementStrategy(StatementStrategy statementStrategy, RowMapper<E> rowMapper) throws RuntimeException {
 		List<E> result = new ArrayList<>();
 		
 		try (
@@ -62,7 +73,23 @@ public class JdbcContext {
 		return result;
 	}
 	
-	private int executeUpdateWithStatementStrategy(StatementStrategy statementStrategy) throws RuntimeException {		
+	private <E> E queryForObjectWithStatementStrategy(StatementStrategy statementStrategy, RowMapper<E> rowMapper) {
+		try (
+			Connection conn = dataSource.getConnection();
+			PreparedStatement pstmt = statementStrategy.makeStatement(conn);
+			ResultSet rs = pstmt.executeQuery();
+		) {
+			if(rs.next()) {
+				return rowMapper.mapRow(rs, rs.getRow());
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		return null;
+	}
+	
+	
+	private int updateWithStatementStrategy(StatementStrategy statementStrategy) throws RuntimeException {		
 		int count = 0;
 		
 		try (
